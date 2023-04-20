250d029ffbd95a7c877e7ce60d0327326f8bbd89
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-04-20 12:07:28 +0530

    Add files via upload

---
+++ Static var.java
@@ -0,0 +1,28 @@
+
+class New
+{
+	static int val=0;
+	//Method
+	void increment()
+	{
+		val++;
+		System.out.print(val);
+	}
+	
+	{
+		val++;
+		System.out.println("After Increment"+val);
+	}
+	
+}
+public class Practical5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		New n1=new New();
+		n1.increment();
+		
+
+	}
+
+}
